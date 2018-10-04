853e68d1f01650762c869f29e4c31b79483e79b6
Christopher Wells <cwellsny@nycap.rr.com>
2018-10-04 18:13:28 -0400

    Add some test cases

---
+++ src/test/scala/minijava/MainSpec.scala
@@ -0,0 +1,96 @@
+package minijava
+
+import minijava.messages.{CompilerError, CompilerMessage, ParsingError}
+import org.scalatest._
+
+class MainSpec extends FlatSpec with Matchers {
+  "parseString" should "should successfully parse the Factorial example" in {
+    val input = Main.readFile("examples/Factorial.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the BinarySearch example" in {
+    val input = Main.readFile("examples/BinarySearch.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the BubbleSort example" in {
+    val input = Main.readFile("examples/BubbleSort.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the TreeVisitor example" in {
+    val input = Main.readFile("examples/TreeVisitor.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the QuickSort example" in {
+    val input = Main.readFile("examples/QuickSort.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the LinearSearch example" in {
+    val input = Main.readFile("examples/LinearSearch.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the LinkedList example" in {
+    val input = Main.readFile("examples/LinkedList.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should successfully parse the BinaryTree example" in {
+    val input = Main.readFile("examples/BinaryTree.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isRight shouldBe true
+  }
+
+  it should "should fail to parse the MissingOpeningCurly example" in {
+    val input = Main.readFile("examples/MissingOpeningCurly.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isLeft shouldBe true
+    output.left.get shouldBe List(CompilerMessage(CompilerError, ParsingError, 2, 4, "missing '{' at 'public'"))
+  }
+
+  it should "should fail to parse the MissingClosingCurly example" in {
+    val input = Main.readFile("examples/MissingClosingCurly.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isLeft shouldBe true
+    output.left.get shouldBe List(CompilerMessage(CompilerError, ParsingError, 10, 0, "missing '}' at 'class'"))
+  }
+
+  it should "should fail to parse the MissingLots example" in {
+    val input = Main.readFile("examples/MissingLots.minijava")
+
+    val output = Main.parseString(input)
+
+    output.isLeft shouldBe true
+  }
+}
---
+++ src/test/scala/minijava/messages/CompilerMessagesSpec.scala
@@ -0,0 +1,27 @@
+package minijava.messages
+
+import org.scalatest._
+
+class CompilerMessagesSpec extends FlatSpec with Matchers {
+  "toDisplayString" should "should properly display a parsing/lexing error" in {
+    val message = CompilerMessage(CompilerError, ParsingError, 2, 4, "missing '{' at 'public'")
+
+    val output = message.toDisplayString()
+    val expected = "%s[parsing/lexing] %s%s[error] %sln:2 col:4 - missing '{' at 'public'".format(
+      Console.YELLOW, Console.RESET, Console.RED, Console.RESET
+    )
+
+    output shouldBe expected
+  }
+
+  it should "should properly display a parsing/lexing warning" in {
+    val message = CompilerMessage(CompilerWarning, ParsingError, 2, 4, "missing '{' at 'public'")
+
+    val output = message.toDisplayString()
+    val expected = "%s[parsing/lexing] %s%s[warning] %sln:2 col:4 - missing '{' at 'public'".format(
+      Console.YELLOW, Console.RESET, Console.YELLOW, Console.RESET
+    )
+
+    output shouldBe expected
+  }
+}
