92215bf01e43cd0cc0fef1cbf757a9cbcd94d714
Christopher Wells <cwellsny@nycap.rr.com>
2018-11-04 20:06:51 -0500

    Have the codegen tests actually run the generated bytecode

--- src/test/scala/minijava/codegeneration/CodeGenerationSpec.scala
+++ src/test/scala/minijava/codegeneration/CodeGenerationSpec.scala
@@ -5,6 +5,8 @@ import java.io.FileOutputStream
 import minijava.Main
 import minijava.typechecking.{MainClassType, TypeChecking}
 import org.scalatest._
+import sys.process._
+
 
 class CodeGenerationSpec extends FlatSpec with Matchers {
   "CodeGenerationVisitor" should "work on the HelloOne example" in {
@@ -25,6 +27,16 @@ class CodeGenerationSpec extends FlatSpec with Matchers {
     val mainClassType = typeTable.get("HelloOne").get.asInstanceOf[MainClassType]
 
     vistor.visitMainClassType("HelloOne.minijava", mainClassType)
+
+    try {
+      val fos = new FileOutputStream("HelloOne.class")
+      try
+        fos.write(vistor.getClassWriter().toByteArray)
+      //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
+      finally if (fos != null) fos.close()
+    }
+
+    testOutput("HelloOne", "1\n")
   }
 
   it should "work on the HelloTwo example" in {
@@ -40,8 +52,6 @@ class CodeGenerationSpec extends FlatSpec with Matchers {
 
     val typeTable = typeCheckResult.right.get
 
-    print(typeTable)
-
     val vistor = new CodeGenerationVisitor()
 
     val mainClassType = typeTable.get("HelloTwo").get.asInstanceOf[MainClassType]
@@ -55,5 +65,15 @@ class CodeGenerationSpec extends FlatSpec with Matchers {
       //fos.close(); There is no more need for this line since you had created the instance of "fos" inside the try. And this will automatically close the OutputStream
       finally if (fos != null) fos.close()
     }
+
+    testOutput("HelloTwo", "2\n")
+  }
+
+  def testOutput(mainClassName: String, expectedOutput: String): Assertion = {
+    val command = f"java $mainClassName"
+
+    val actualOutput = command !!
+
+    actualOutput shouldBe expectedOutput
   }
 }
