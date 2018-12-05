2927188ece043f14c8efb1f02d91ab797b54ec2c
Christopher Wells <cwellsny@nycap.rr.com>
2018-12-04 22:17:04 -0500

    Get more of the logic of null check optimization working

--- src/main/scala/minijava/optimization/NullCheckAnalysis.scala
+++ src/main/scala/minijava/optimization/NullCheckAnalysis.scala
@@ -2,16 +2,15 @@ package minijava.optimization
 
 import java.util
 
-import soot.Value
+import soot.Local
 import soot.jimple.NullConstant
 import soot.jimple.internal.{ConditionExprBox, ImmediateBox}
 import soot.toolkits.graph.DirectedGraph
 import soot.toolkits.scalar.ForwardFlowAnalysis
 
-import collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
 
-class NullCheckAnalysis(graph: DirectedGraph[soot.Unit]) extends ForwardFlowAnalysis[soot.Unit, util.ArrayList[soot.Unit]](graph) {
+class NullCheckAnalysis(graph: DirectedGraph[soot.Unit]) extends ForwardFlowAnalysis[soot.Unit, util.HashSet[Local]](graph) {
   private val unitsToRemove: ArrayBuffer[soot.Unit] = new ArrayBuffer[soot.Unit]()
 
   doAnalysis()
@@ -20,48 +19,31 @@ class NullCheckAnalysis(graph: DirectedGraph[soot.Unit]) extends ForwardFlowAnal
     unitsToRemove
   }
 
-  override def flowThrough(in: util.ArrayList[soot.Unit], d: soot.Unit, out: util.ArrayList[soot.Unit]): Unit = {
-    println(in)
-
-    println(d)
-
-    /*for (b <- d.getUseBoxes.asScala) {
-      b match {
-        case b: ConditionExprBox =>
-          println(b)
-
-          unitsToRemove.append(d)
-        case _ => ()
-      }
-    }*/
-
+  override def flowThrough(in: util.HashSet[Local], d: soot.Unit, out: util.HashSet[Local]): Unit = {
     if (isNullCheckCondition(d)) {
-      unitsToRemove.append(d)
+      val nullCheckedObj = getNullCheckedLocal(d)
+
+      if (in.contains(nullCheckedObj)) {
+        unitsToRemove.append(d)
+      }
+
+      out.add(nullCheckedObj)
     }
 
-    println(d.getUseAndDefBoxes)
-    println(d.getUseAndDefBoxes.size())
-
-    println("")
-
     copy(in, out)
-    out.add(d)
   }
 
-  override def newInitialFlow(): util.ArrayList[soot.Unit] = {
-    new util.ArrayList[soot.Unit]()
+  override def newInitialFlow(): util.HashSet[Local] = {
+    new util.HashSet[Local]()
   }
 
-  override def merge(in1: util.ArrayList[soot.Unit], in2: util.ArrayList[soot.Unit], out: util.ArrayList[soot.Unit]): Unit = {
-    println("~~~~~")
-    println("Merge")
-    println("~~~~~")
-
+  override def merge(in1: util.HashSet[Local], in2: util.HashSet[Local], out: util.HashSet[Local]): Unit = {
+    // Intersection, since this is a must analysis
     out.addAll(in1)
-    out.addAll(in2)
+    out.retainAll(in2)
   }
 
-  override def copy(source: util.ArrayList[soot.Unit], dest: util.ArrayList[soot.Unit]): Unit = {
+  override def copy(source: util.HashSet[Local], dest: util.HashSet[Local]): Unit = {
     dest.addAll(source)
   }
 
@@ -70,5 +52,9 @@ class NullCheckAnalysis(graph: DirectedGraph[soot.Unit]) extends ForwardFlowAnal
       unit.getUseAndDefBoxes.get(2).isInstanceOf[ConditionExprBox] &&
       unit.getUseAndDefBoxes.get(1).isInstanceOf[ImmediateBox] &&
       unit.getUseAndDefBoxes.get(1).asInstanceOf[ImmediateBox].getValue.eq(NullConstant.v())
+  }
+
+  private def getNullCheckedLocal(unit: soot.Unit): Local = {
+    unit.getUseAndDefBoxes.get(0).getValue.asInstanceOf[Local]
   }
 }
